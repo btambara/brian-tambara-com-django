20c6059ef070d447ef8d4c65a1fc6daff4f15f8a
Brian Tambara <btambara@gmail.com>
2023-09-12 10:56:14 -0700

    add professional profile view

--- www/src/components/Introduction.vue
+++ www/src/components/Introduction.vue
@@ -2,7 +2,7 @@
   <div class="intro">
     <img src="../assets/code.svg" class="logo" alt="Code logo" />
     <h1>Hi! I'm Brian</h1>
-    <p>I am a Los Angeles based Software Engineer currently looking for work.</p>
+    <p>I am a Los Angeles based Software Engineer currently looking for work. My preferred languages for building web applications are Python, Java, and Typescript.</p>
   </div>
 </template>
 
