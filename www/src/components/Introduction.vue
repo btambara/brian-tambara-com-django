752e4376bbe4aa85852143f9179dfbc97f9ce942
Brian Tambara <btambara@gmail.com>
2023-09-14 09:23:06 -0700

    fix mobile view

--- www/src/components/Introduction.vue
+++ www/src/components/Introduction.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="col">
-      <img src="../assets/code.svg" class="logo" alt="Code logo" />
-    </div>
-    <div class="col text-start">
-      <h1>Hi! I'm Brian</h1>
-      <p>I am a Los Angeles based Software Engineer currently looking for work. My preferred languages for building web applications are Python, Java, and Typescript.</p>
-    </div>
+  <div class="col-12 col-xl-4">
+    <img src="../assets/code.svg" class="logo" alt="Code logo" />
+  </div>
+  <div class="col-12 text-start col-xl-5">
+    <h1>Hi! I'm Brian</h1>
+    <p>I am a Los Angeles based Software Engineer currently looking for work. My preferred languages for building web applications are Python, Java, and Typescript.</p>
+  </div>
 </template>
 
 <style scoped>
 .logo {
-  height: 20em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+height: 20em;
+padding: 1.5em;
+will-change: filter;
+transition: filter 300ms;
 }
 </style>
