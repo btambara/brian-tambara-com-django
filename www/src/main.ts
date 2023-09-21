bdb3e53b734b889819a5657db5dc15fca52212d4
Brian Tambara <btambara@gmail.com>
2023-09-21 08:55:01 -0700

    remove app vue, update .env DEBUG (#13)

--- www/src/main.ts
+++ www/src/main.ts
@@ -1,12 +1,10 @@
 import 'vite/modulepreload-polyfill';
 import { createApp, } from 'vue'
 import './scss/style.scss'
-import App from './views/App.vue'
 import Index from './views/Index.vue'
 import Resume from './views/Resume.vue'
 import 'bootstrap'
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-createApp(App).mount('#app')
 createApp(Index).mount('#index')
 createApp(Resume).mount('#resume')
